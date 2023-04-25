350ccca252dd92ae3decd9aee176941c34c82931
COST <COST@COST>
2023-04-25 13:15:56 +0300

    Add project files.

---
+++ AyzMuhasebeServer.Presentation/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using AyzMuhasebeServer.Presentation.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AyzMuhasebeServer.Presentation.Controllers
+{
+    public sealed class TestController :ApiController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("İşlem Başarılı");
+        }
+    }
+}
